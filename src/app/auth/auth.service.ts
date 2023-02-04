3a01d7eff82cf294655c54ea1605fd5e935a1119
galaxyuliana <yulianashin613@gmail.com>
2023-02-03 22:49:43 -0800

    Added AuthResponseData interface

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,15 +1,27 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 
+interface AuthResponseData {
+    kind: string;
+    idToken: string;
+    email: string;
+    refreshToken: string;
+    expiresIn: string;
+    localId: string;
+}
+
 @Injectable({providedIn: 'root'})
 export class AuthService {
     constructor(private http: HttpClient) {}
     
     signup(email: string, password: string) {
-        this.http.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyA5r4ssAQ2xQbqIohhyDSz8Iavz9oZGifE', {
-            email: email,
-            passworkd: password,
-            returnSecureToken: true
-        });
+        return this.http.post<AuthResponseData>(
+            'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyA5r4ssAQ2xQbqIohhyDSz8Iavz9oZGifE',
+            {
+                email: email,
+                passworkd: password,
+                returnSecureToken: true
+            }
+        );
     }
 }
