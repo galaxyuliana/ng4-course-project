113cb65ba035a449932e4036c20090645581ebe1
galaxyuliana <yulianashin613@gmail.com>
2023-02-07 22:44:50 -0800

    Improved error handling

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -34,13 +34,9 @@ export class AuthComponent implements OnInit {
       this.authService.signup(email, password).subscribe(resData => {
         console.log(resData);
         this.isLoading = false;
-      }, errorRes => {
-        console.log(errorRes);
-        switch(errorRes.error.error.message) {
-          case 'EMAIL_EXISTS':
-            this.error = 'This email exists already';
-        }
-        // this.error = 'An error occurred!';
+      }, errorMessage => {
+        console.log(errorMessage);
+        this.error = errorMessage
         this.isLoading = false;
       });
     }
