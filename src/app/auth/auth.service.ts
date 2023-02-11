8a141e281e8e47471d921524548456e77236bcf7
galaxyuliana <yulianashin613@gmail.com>
2023-02-10 23:45:44 -0800

    login error handling

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -44,7 +44,14 @@ export class AuthService {
         }
         switch(errorRes.error.error.message) {
             case 'EMAIL_EXISTS':
-            errorMessage = 'This email exists already';
+                errorMessage = 'This email exists already';
+                break;
+            case 'EMAIL_NOT_FOUND':
+                errorMessage = 'This email does not exist.';
+                break;
+            case 'INVALID_PASSWORD':
+                errorMessage = 'This password is not correct.';
+                break;
         }
         return throwError(errorMessage)
     }
