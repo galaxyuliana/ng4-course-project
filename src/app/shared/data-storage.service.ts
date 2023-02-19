c5726e5bc1495ac97b55036bd5db74b87ee58663
galaxyuliana <yulianashin613@gmail.com>
2023-02-18 23:08:07 -0800

    reformat

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -27,15 +27,17 @@ export class DataStorageService {
   }
 
   fetchRecipes() {
-    return this.authService.user.pipe(take(1), exhaustMap(user => {
-      return this.http
-        .get<Recipe[]>(
-          'https://ng-course-recipe-book-129a9-default-rtdb.firebaseio.com/recipes.json',
-          {
-            params: new HttpParams().set('auth', user.token)
-          }
-        );
-    }),
+    return this.authService.user.pipe(
+      take(1),
+      exhaustMap(user => {
+        return this.http
+          .get<Recipe[]>(
+            'https://ng-course-recipe-book-129a9-default-rtdb.firebaseio.com/recipes.json',
+            {
+              params: new HttpParams().set('auth', user.token)
+            }
+          );
+      }),
       map(recipes => {
         return recipes.map(recipe => {
           return {
