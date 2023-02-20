66c63efb2ea75b4ff0198266ec6ad639e9ecf9c5
galaxyuliana <yulianashin613@gmail.com>
2023-02-20 00:03:45 -0800

    Updated AuthInterceptorService

--- src/app/auth/auth-interceptor.service.ts
+++ src/app/auth/auth-interceptor.service.ts
@@ -1,9 +1,20 @@
-import { HttpInterceptor } from "@angular/common/http";
+import { HttpHandler, HttpInterceptor, HttpParams, HttpRequest } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { exhaustMap, take } from "rxjs";
+import { AuthService } from "./auth.service";
 
 @Injectable()
 export class AuthInterceptorService implements HttpInterceptor {
+    constructor(private authService: AuthService) {}
+
     intercept(req: HttpRequest<any>, next: HttpHandler) {
-        return null;
+        this.authService.user.subscribe();
+        return this.authService.user.pipe(
+            take(1),
+            exhaustMap(user => {
+                const modifiedReq = req.clone({params: new HttpParams});
+                return next.handle(req);
+            })
+        );
     }
 }
