bf60d7376ce293c159f6c87c426ef4526f3c16ae
galaxyuliana <yulianashin613@gmail.com>
2023-03-03 23:36:39 -0800

    Imported alert component

--- src/app/shared/alert/alert.component.ts
+++ src/app/shared/alert/alert.component.ts
@@ -1,9 +1,10 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 @Component({
     selector: 'app-alert',
-    templateUrl: './alert.component.html'
+    templateUrl: './alert.component.html',
+    styleUrls: ['.alert.component.css']
 })
 export class AlertComponent {
-
+    @Input() message: string;
 }
