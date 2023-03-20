4608daf6df20a2179788fb3623a09f8fad20c2f3
galaxyuliana <yulianashin613@gmail.com>
2023-03-19 23:56:20 -0700

    Updated RecipesModule

--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -1,4 +1,6 @@
+import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
 import { RecipeDetailComponent } from "./recipe-detail/recipe-detail.component";
 import { RecipeEditComponent } from "./recipe-edit/recipe-edit.component";
 import { RecipeItemComponent } from "./recipe-list/recipe-item/recipe-item.component";
@@ -14,6 +16,10 @@ import { RecipesComponent } from "./recipes.component";
         RecipeItemComponent,
         RecipeListComponent,
         RecipeStartComponent,
+    ],
+    imports: [
+        RouterModule,
+        CommonModule
     ],
     exports: [
         RecipesComponent,
