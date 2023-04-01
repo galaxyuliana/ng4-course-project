2d3d0bf0ad03a57becee8ae6da22cb693b66ecc1
galaxyuliana <yulianashin613@gmail.com>
2023-03-31 22:50:10 -0700

    Lazy load shopping-list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,10 @@ const appRoutes: Routes = [
     { path: '', redirectTo: '/recipes', pathMatch: 'full' },
     { path: 'recipes', loadChildren: () => 
         import('./recipes/recipes.module').then((m) => m.RecipesModule)
-    }
+    },
+    { path: 'shopping-list', loadChildren: () => 
+        import('./shopping-list/shopping-list.module').then((m) => m.ShoppingListModule)
+    },
     // Legacy
     // { path: 'recipes', loadChildren: './recipes/recipes.module#RecipesModule' }
 ];
